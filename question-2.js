62a33266da4f918c1b385cbc314596669f3fa975
lakhdar hafsi <40460234+lacdart2@users.noreply.github.com>
2020-08-30 23:35:58 +0200

    Create question-2.js

---
+++ question-2.js
@@ -0,0 +1,47 @@
+// answer for question 2
+
+
+  const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+  
+  const resultsContainer = document.querySelector(".results");
+  const name = document.querySelector(".name");
+  const rating = document.querySelector(".rating");
+  const tags = document.querySelector(".tags");
+  
+
+  async function getGames() {
+  
+  try {
+  const gamesResponse = await fetch (url);
+  const gamesResults = await gamesResponse.json();
+ const gamesData = gamesResults.all;
+  
+  resultsContainer.innerHTML = "";
+for (let i = 0; i < gamesData.length; i++) {
+    console.log(gamesData[i].name);
+     console.log(gamesData[i].rating)
+      console.log(gamesData[i].tags[i])
+
+    if (i === 8) {
+      break;
+    }
+
+    resultsContainer.innerHTML += `<div class="result">${gamesData[i].name}</div>`;
+    resultsContainer.innerHTML += `<div class="result">${gamesData[i].rating}</div>`;
+     resultsContainer.innerHTML += `<div class="result">${gamesData[i].tags}</div>`;
+  }  
+  catch (error) {
+        console.log("An error occurred");
+        resultsContainer.innerHTML = ErrorMessage("An error occurred when calling the API");
+    }
+
+}
+getGames();
+  
+  
+  
+  
+  
+  
+ 
+  
